69c921808435cc89d8a3eb4354df5b2557a2bb22
Anastasia <splashafleck@gmail.com>
2024-05-08 16:41:42 +0300

    Database change

--- API_Project/urls.py
+++ API_Project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from pereval import views
+from .yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
 router.register(r'pereval', views.PerevalAddedViewset, 'PerevalAdded')
@@ -27,3 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
+
+urlpatterns += doc_urls
