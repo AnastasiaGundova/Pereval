c5168e287aacdda1694e49afea5a9c33da4fac2d
Anastasia <splashafleck@gmail.com>
2024-05-09 14:09:35 +0300

    last cahnges

--- API_Project/yasg.py
+++ API_Project/yasg.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import re_path, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,7 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui, name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui, name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger'), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc'), name='schema-redoc'),
 ]
